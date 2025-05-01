b4bc75201e06099c1cfe2cb73e94876d8bd461b3
hogesako <43780506+hogesako@users.noreply.github.com>
2025-05-01 21:23:02 +0900

    root対応

--- devcontainers_install.sh
+++ devcontainers_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-if [ ! -f ~/.bash_aliases ]; then
+USER=`whoami`
+echo "${USER}"
+if [ "${USER}" == "root" ]; then
+    command cat .bash_aliases >> /root/.bashrc
+else if [ ! -f ~/.bash_aliases ]; then
     command ln -snf `pwd`/.bash_aliases ~/.bash_aliases
 fi
